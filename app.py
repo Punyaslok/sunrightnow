264249c4885ca5700509c9660601545705c366f3
Punyaslok Pattnaik <punyaslokpattnaik@yahoo.co.in>
2017-07-20 05:27:36 +0530

    Debugging

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ def search_in_db(client=None, input_date=None):
         return entry.image_path
 
 def populate_db():
-    start_date = '2017-03-03'
+    start_date = '2017-03-05'
     end_date = '2017-03-05'
     plot_hmi_for_range(start_date, end_date, 'magnetogram')
     plot_hmi_for_range(start_date, end_date, 'continuum')
